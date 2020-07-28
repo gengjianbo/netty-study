5e045c27a5e807e625935181bd4476ab258bc1c1
gengjianbo <jianbo_geng@163.com>
2020-07-28 18:18:40 +0800

    test bio

--- src/main/java/com/jianbo/bio/BIOServer.java
+++ src/main/java/com/jianbo/bio/BIOServer.java
@@ -1,8 +1,46 @@
 package com.jianbo.bio;
 
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
 public class BIOServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("服务器启动...");
+        while (true){
+            Socket accept = serverSocket.accept();
+            System.out.println("新客户端连接："+accept.getPort());
+            new Thread(() -> {
+                try {
+                    handler(accept);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+
+    public static void handler(Socket accept) throws Exception {
+        InputStream inputStream = accept.getInputStream();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+        while (true){
+            byte[] b = new byte[1];
+            int read = inputStream.read(b);
+            byteBuffer.put(b);
+            if (read == 0 ) {
+                byteBuffer.flip();
+                String s = new String(byteBuffer.array(), byteBuffer.position(), byteBuffer.limit());
+                System.out.println("收到客户端数据：" + s);
+            }
+            if (read == -1){
+                System.out.println("客户端失去连接....");
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/com/jianbo/bio/Test.java
@@ -0,0 +1,7 @@
+package com.jianbo.bio;
+
+public class Test {
+  public static void main(String[] args) {
+
+  }
+}
